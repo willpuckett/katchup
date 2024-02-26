c4c9dbbe10c55b374774b0134615e8b44d0577b4
Will Puckett <willpuckett@gmail.com>
2024-02-26 14:28:06 -0800

    add expect script to update .config files

--- katchup.sh
+++ katchup.sh
@@ -1,43 +1,33 @@
 #!/bin/bash
 
 main () {
-  pull=true
-  while getopts m:n flag
-  do
-      case "${flag}" in
-          m) target_mcu=${OPTARG};;
-          n) pull=false;;
-          # ) fullname=${OPTARG};;
-      esac
-  done
   # Make sure pv is installed (for progress bars)
   check_pv 
 
-  echo "Let's katchup $1..."
+  echo "Katching up $fab..."
 
-  # Call the functions. Pull checks for available updates and exits if there are none...
+  # Pull checks for available updates and exits if there are none...
     if [[ $pull == true ]]; then
       echo Pulling!
-      pull $1
+      pull
   fi
 
   sudo service klipper stop
-  # Then this for loop updates eckh mcu with a config in the ~/kup directory
-  for config in ~/printer_data/config/katchup/$1/*.config; do
-    echo config $config target_mcu $target_mcu mcu $mcu
-    if [[ -z $mcu || $config =~ $target_mcu ]]; then
+  # Then this for loop updates each mcu with a config in the ~/printer_data/config/katchup/$fab directory
+  for config in ~/printer_data/config/katchup/$fab/*.config; do
+    echo config $config target_mcu $target_mcu 
+    if [[ -z $target_mcu || $config =~ $target_mcu ]]; then
       echo $config
-      flashy $1 "$config"
+      flashy "$config"
     fi
   done
   sudo service klipper start && sleep 5
 
-  # And finally a firmware restart because why not
-  echo 🦒 Executing Firmware Restart...
-  echo FIRMWARE_RESTART > ~/printer_data/comms/klippy.serial 
+  # # And finally a firmware restart because why not
+  # echo 🦒 Executing Firmware Restart...
+  # echo FIRMWARE_RESTART > ~/printer_data/comms/klippy.serial 
   echo "✅ You're all up to date. "
 }
-
 
 check_pv () {
   has_pv=$(which pv)
@@ -50,12 +40,11 @@ check_pv () {
     echo Sorry, couldn\'t install pv. Try manually installing it and run again.
     exit
   fi
-
 }
 
 # Bring your remote refs up to date (git remote update). Then, git status -uno will tell you whether the branch you are tracking is ahead, behind or has diverged. If it says nothing, the local and remote are the same. 
 pull () {
-  cd ~/$1
+  cd ~/$fab
   git remote update > /dev/null
   behind=$(git status -uno | grep behind)
 
@@ -67,19 +56,32 @@ pull () {
   fi
 }
 
-# flashy ~/katchup/mcu-canID-usbID.config
+# flashy ~/printer_data/config/katchup/$fab/mcu-canID-usbID.config
 # for usb only devices, omit canID but use double --
-# ie ~/katchup/mcu--usbID.config
+# ie ~/printer_data/config/katchup/$fab/mcu--usbID.config
 flashy () {
-  config="${2##*/}"
+  flashtool="~/klippy-env/bin/python3 ~/katapult/scripts/flashtool.py"
+  config="${1##*/}"
   IFS=- read mcu can usb <<< "${config%.config}"
 
   echo -e "\nBuilding for $mcu..."
   
   # Clean up from last go 'round, and copy in the .config file
-  cd ~/$1
+  cd ~/$fab
   make clean
-  cp "$2" ~/$1/.config
+  cp "$1" ~/$fab/.config
+
+  # Call make menuconfig to update the .config file
+  expect -c '
+  set timeout -1
+  spawn make menuconfig
+  match_max 100000
+  send -- "q"
+  expect "Quit"
+  send -- "y"
+  ' 
+  cp ~/$fab/.config "$1"
+
   # build the config
   make -j4 |  pv --line-mode --size 55 --eta --progress > /dev/null
   echo -e "finished building!\nflashing $mcu..."
@@ -92,29 +94,33 @@ flashy () {
   
   # Put canbus bridge devices in flash mode...
   if [[ -n "$usb" ]] && [[ -n "$can" ]]; then 
-	  ~/klippy-env/bin/python3 ~/katapult/scripts/flashtool.py -r -u "$can" 2> /dev/null
+	  $flashtool -r -u "$can" 2> /dev/null
 	  sleep 5
   fi
   
   # Flash
-  ~/klippy-env/bin/python3 ~/katapult/scripts/flashtool.py $( [[ -n "$usb" ]] \
+  $flashtool $( [[ -n "$usb" ]] \
       && echo "-d /dev/serial/by-id/$usb" || echo "-u $can" ) 
   sleep 5
 } 
 
-while getopts ba flag
+export pull=true
+while getopts bam:n flag
   do
       case "${flag}" in
-          b) fab=katapult;;
-          a) fab=klipper;;
+          b) export fab=katapult;;
+          a) export fab=klipper;;
+          m) export target_mcu=${OPTARG};;
+          n) pull=false;;
           # ) fullname=${OPTARG};;
       esac
   done
+
 
 if [[ -z $fab ]]; then
   echo -e "Please specify a firmware to katchup. \nie: katchup.sh -b for katapult (bootloader) \nor katchup.sh -a for klipper (application)"
   exit
 fi
 
-main $fab "$@"; exit
+main; exit
 # echo $fab
